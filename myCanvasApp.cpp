ff3c5fd9aefb39cd433af830da6526cc4f1507d0
Daniil <danah29122003@gmail.com>
2024-11-13 20:50:04 +0300

    Базочка

--- myCanvasApp.cpp
+++ myCanvasApp.cpp
@@ -1,12 +1,29 @@
 ﻿#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <vector>
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "My Canvas App", sf::Style::Titlebar | sf::Style::Close);
 
 
-    shape.setFillColor(sf::Color::Green);
+    sf::RectangleShape myCanvas(sf::Vector2f(800, 620));
+    myCanvas.setFillColor(sf::Color::White);
+    myCanvas.setPosition(sf::Vector2f(430, 50));
+    myCanvas.setOutlineColor(sf::Color(50, 50, 50));
+    myCanvas.setOutlineThickness(3);
+
+    std::vector<sf::CircleShape> points;
+
+    sf::Image cursorImage;
+    if (!cursorImage.loadFromFile("C:/cpp projects/myCanvasApp/Resources/cursor.png")) {
+        return -1;
+    }
+
+    sf::Cursor cursor;
+    cursor.loadFromPixels(cursorImage.getPixelsPtr(), cursorImage.getSize(), sf::Vector2u(0, 0));
+
+    window.setMouseCursor(cursor);
 
     while (window.isOpen())
     {
@@ -15,10 +32,17 @@ int main()
         {
             if (event.type == sf::Event::Closed)
                 window.close();
+
         }
 
-        window.clear();
-        window.draw(shape);
+
+
+
+        window.clear(sf::Color(200, 200, 200));
+        window.draw(myCanvas);
+
+        
+
         window.display();
     }
 
